50f4bb35f0963852c6958a5a1bdf3389e948d60a
Sarkraf PC <sarkraf.dev@gmail.com>
2024-12-17 00:00:16 +0100

    fix 154 offenses (rubocop)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,9 +8,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :password, :password_confirmation])
-    devise_parameter_sanitizer.permit(:account_update, keys: [:email, :password, :password_confirmation, :current_password])
-
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :email, :password, :password_confirmation ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :email, :password, :password_confirmation, :current_password ])
   end
 
   def after_sign_in_path_for(resource)
--- db/seeds.rb
+++ db/seeds.rb
@@ -8,4 +8,4 @@
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
 
-User.create!(email: 'alex@mail.com',password:"coucou")
+User.create!(email: 'alex@mail.com', password: "coucou")
